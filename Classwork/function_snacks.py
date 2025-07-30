6a0b3531ddd81070b65cecae268a8691aaa8d4ce
Silver36-ship-it <emycallistus@gmail.com>
2025-07-30 23:29:49 +0100

    updated file

--- Classwork/function_snacks.py
+++ Classwork/function_snacks.py
@@ -1,10 +1,20 @@
 import math
-def divide_or_square(number):
-	if type(number) == str or number < 0:
+def divide_or_square (number):
+	number = float(number)
+	if type(number) == str or number <= 0:
 		raise ValueError
 	elif number % 5 == 0: 
 		return round(math.sqrt(number),2)
 	else:
-		return number % 5
+		return round(number % 5,2)
+		
+def future_investment(investment_amount,monthly_interestRate,years):
+	if type(investment_amount) == str or type(monthly_interestRate) == str or type(years) == str:
+		raise ValueError
+	convert_to_months = years * 12
+	interest_rate_conversion = monthly_interestRate / 100 
+	future_investment_value = investment_amount * (1 + interest_rate_conversion)**convert_to_months
+	return round(future_investment_value,2)
+	
 		
 	  
--- Classwork/test_function_snacks.py
+++ Classwork/test_function_snacks.py
@@ -14,7 +14,29 @@ class TestFunctionSnacks(unittest.TestCase):
 		
 	def test_number_is_not_a_negative_number(self):
 		self.assertRaises(ValueError,function_snacks.divide_or_square,-9)
+		
+	def test_number_accepts_decimal_numbers(self):
+		result = function_snacks.divide_or_square(27.8)
+		self.assertEqual(result,2.8)
+
+class TestFutureInvestment(unittest.TestCase):
+	def test_future_investment_value(self):
+		function_snacks.future_investment(1,2,3)
+	
+	def test_future_investment_value_calculation(self):
+		result = function_snacks.future_investment(100,50,1)
+		self.assertEqual(result,12974.63)
+		
+	def test_that_prinicipal_amount_is_not_string(self):
+		self.assertRaises(ValueError,function_snacks.future_investment,"string",34,56)
+	
+	def test_that_monthly_interestRate_is_not_string(self):	
+		self.assertRaises(ValueError,function_snacks.future_investment,23,"string",67)
+	
+	def test_that_the_year_input_is_not_string(self):
+		self.assertRaises(ValueError,function_snacks.future_investment,34,68,"string")
 
 		
+				
 
 		
